19b0f1b6478f5be366960baf134482f7a2642b49
michaelmikemikemike <fortnitefrenzy.69z@gmail.com>
2025-09-26 02:24:55 -0400

    Update apply_relu.m

--- apply_relu.m
+++ apply_relu.m
@@ -1,3 +1,9 @@
 function outarray = apply_relu(inarray)
+% APPLY_RELU  Elementwise ReLU activation
+% Input:
+%   inarray  - Input array (any size)
+% Output:
+%   outarray - Same size as input, with negative values set to 0
+
 outarray = max(inarray, 0);
 end
