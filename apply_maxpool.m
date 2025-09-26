7e87e654e8d2b1bc07b51c2bac871fc2c02da67a
michaelmikemikemike <fortnitefrenzy.69z@gmail.com>
2025-09-26 02:24:08 -0400

    Update apply_maxpool.m

--- apply_maxpool.m
+++ apply_maxpool.m
@@ -1,4 +1,12 @@
 function outarray = apply_maxpool(inarray)
+
+% APPLY_MAXPOOL  2x2 max pooling with stride 2
+%
+% Input:
+%   inarray  - Input feature map, size H x W x D
+% Output:
+%   outarray - Pooled output, size (H/2) x (W/2) x D
+
 [inH, inW, inD] = size(inarray);
 outH = inH / 2;
 outW = inW / 2;
@@ -7,6 +15,7 @@ outarray = zeros(outH, outW, inD);
 for d = 1:inD
     for i = 1:outH
         for j = 1:outW
+            % Take 2x2 patch and keep the maximum
             patch = inarray(2*i-1:2*i, 2*j-1:2*j, d);
             outarray(i,j,d) = max(patch(:));
         end
