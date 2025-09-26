139751f8464a1ab6584525f3c1a527de0be7d79b
michaelmikemikemike <fortnitefrenzy.69z@gmail.com>
2025-09-26 19:26:07 -0400

    Update demo_runCNN.m

--- demo_runCNN.m
+++ demo_runCNN.m
@@ -1,11 +1,11 @@
 /CNNProject
     demo_runCNN.m            % main driver script
-    apply_imnormalize.m
-    apply_relu.m
-    apply_maxpool.m
-    apply_convolve.m
-    apply_fullconnect.m
-    apply_softmax.m
-    CNNparameters.mat
-    cifar10testdata.mat
+    apply_imnormalize.m      % image normalization layer
+    apply_relu.m             % convolution layer
+    apply_maxpool.m          % ReLU activation
+    apply_convolve.m         % max pooling
+    apply_fullconnect.m      % fully connected layer
+    apply_softmax.m          % softmax output layer
+    CNNparameters.mat        % network weights and baises
+    cifar10testdata.mat      % CIFAR-10 test images and labels
     README.txt               % explains how to run demo
